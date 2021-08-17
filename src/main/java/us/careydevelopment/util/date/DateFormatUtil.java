f608636fc1e114dcc4d4f5ced836e58ab089ad82
careydevelopment <mrbrianmcarey@gmail.com>
2021-08-17 19:00:16 -0400

    update for ISO8601

--- src/main/java/us/careydevelopment/util/date/DateFormatUtil.java
+++ src/main/java/us/careydevelopment/util/date/DateFormatUtil.java
@@ -5,10 +5,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateFormatUtil {
-
+    
     public static final DateFormat JUST_DATE_FORMAT = new SimpleDateFormat("MM/dd/yy");
     public static final DateFormat TIME_OF_DAY_FORMAT = new SimpleDateFormat("HH:mm");
     public static final DateFormat JUST_HOUR_FORMAT = new SimpleDateFormat("hha");
+    public static final DateFormat FORMAT_8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
     
     public static String getDate(Long time) {
         return JUST_DATE_FORMAT.format(new Date(time));
@@ -16,5 +17,18 @@ public class DateFormatUtil {
     
     public static String getHourOfDay(Long time) {
         return JUST_HOUR_FORMAT.format(new Date(time));
+    }
+    
+    public static Long getTime(String dateString, DateFormat dateFormat) {
+        Long time = 0l;
+        
+        try {
+            Date d = dateFormat.parse(dateString);
+            time = d.getTime();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return time;
     }
 }
